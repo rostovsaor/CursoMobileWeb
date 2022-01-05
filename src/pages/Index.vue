967964297e5668cd184b2198ec6788bd89339cda
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-01-05 21:28:50 +0200

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex column flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <h3 class="q-mt-lg text-primary">Salvador Anselmo de Oliveira Rombe</h3>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
+
+<style scoped>
+h3 {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
